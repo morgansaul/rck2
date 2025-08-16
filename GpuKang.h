3465885a74ba53211e1c02778adad9126e891ea9
morgansaul <morgansaul00@gmail.com>
2025-08-16 13:40:05 -0400

    Update GpuKang.h

--- GpuKang.h
+++ GpuKang.h
@@ -7,69 +7,67 @@
 #pragma once
 
 #include "Ec.h"
+#include <vector>
 
-#define STATS_WND_SIZE	16
+#define STATS_WND_SIZE      16
 
 struct EcJMP
 {
-	EcPoint p;
-	EcInt dist;
+    EcPoint p;
+    EcInt dist;
 };
 
 //96bytes size
 struct TPointPriv
 {
-	u64 x[4];
-	u64 y[4];
-	u64 priv[4];
+    u64 x[4];
+    u64 y[4];
+    u64 priv[4];
 };
 
 class RCGpuKang
 {
 private:
-	bool StopFlag;
-	EcPoint PntToSolve;
-	int Range; //in bits
-	int DP; //in bits
-	Ec ec;
+    bool StopFlag;
+    std::vector<EcPoint> PntsToSolve;
+    int Range; //in bits
+    int DP; //in bits
+    Ec ec;
 
-	u32* DPs_out;
-	TKparams Kparams;
+    u32* DPs_out;
+    TKparams Kparams;
 
-	EcInt HalfRange;
-	EcPoint PntHalfRange;
-	EcPoint NegPntHalfRange;
-	TPointPriv* RndPnts;
-	EcJMP* EcJumps1;
-	EcJMP* EcJumps2;
-	EcJMP* EcJumps3;
+    EcInt HalfRange;
+    EcPoint PntHalfRange;
+    EcPoint NegPntHalfRange;
+    TPointPriv* RndPnts;
+    EcJMP* EcJumps1;
+    EcJMP* EcJumps2;
+    EcJMP* EcJumps3;
 
-	EcPoint PntA;
-	EcPoint PntB;
+    EcPoint PntA;
+    EcPoint PntB;
 
-	int cur_stats_ind;
-	int SpeedStats[STATS_WND_SIZE];
+    int cur_stats_ind;
+    int SpeedStats[STATS_WND_SIZE];
 
-	void GenerateRndDistances();
-	bool Start();
-	void Release();
-#ifdef DEBUG_MODE
-	int Dbg_CheckKangs();
-#endif
+    void GenerateRndPnts();
+    void SetKangParams();
+
 public:
-	int persistingL2CacheMaxSize;
-	int CudaIndex; //gpu index in cuda
-	int mpCnt;
-	int KangCnt;
-	bool Failed;
-	bool IsOldGpu;
+    RCGpuKang();
+    ~RCGpuKang();
+    int CudaIndex;
+    int mpCnt;
+    bool IsOldGpu;
+    int persistingL2CacheMaxSize;
+    u64 dbg[1024];
 
-	int CalcKangCnt();
-	bool Prepare(EcPoint _PntToSolve, int _Range, int _DP, EcJMP* _EcJumps1, EcJMP* _EcJumps2, EcJMP* _EcJumps3);
-	void Stop();
-	void Execute();
+    int CalcKangCnt();
 
-	u32 dbg[256];
-
-	int GetStatsSpeed();
+    //executes in main thread
+    bool Prepare(std::vector<EcPoint>& _PntsToSolve, int _Range, int _DP, EcJMP* _EcJumps1, EcJMP* _EcJumps2, EcJMP* _EcJumps3);
+    void Execute();
+    int GetStatsSpeed();
+    void Stop() { StopFlag = true; };
 };
